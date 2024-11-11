caf012d090cd1b3e036c06e23d0d9baaef76ee8d
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-11-11 23:19:27 +0100

    delete and change motion doesn't send to clipboard in normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+-- Prevent 'c' and 'x' from sending to the clipboard
+vim.keymap.set("n", "c", '"_c', { noremap = true, silent = true })
+vim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
