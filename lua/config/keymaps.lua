2bb65d1e2c21f77b2a28ec0e743071dcf3468dbb
vuthanhtung2412 <58302728+vuthanhtung2412@users.noreply.github.com>
2024-11-11 23:26:12 +0100

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
--- Prevent 'c' and 'x' from sending to the clipboard
+-- Prevent change and delete motion from sending to the clipboard in normal mode
 vim.keymap.set("n", "c", '"_c', { noremap = true, silent = true })
 vim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
 vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
