1d64855dc7bf0c8b2ae6c09faa000f42e1fcd350
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-14 01:26:31 +0100

    gx open relative to active buffer instead of root dir

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@ vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.opt.winbar = "%=%m %f"
 vim.g.autoformat = false
+-- https://www.reddit.com/r/vim/comments/7xs8x3/opening_local_relative_paths_with_gx/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+vim.keymap.set("n", "gx", function()
+  local file_path = vim.fn.expand("<cfile>")
+  local dir = vim.fn.expand("%:p:h")
+  vim.cmd("silent !cd " .. dir .. " && open " .. file_path)
+end, { silent = true })
