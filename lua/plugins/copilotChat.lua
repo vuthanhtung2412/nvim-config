642471d8e74a0b0e333847d6ad58d23a2e31f132
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-21 23:24:08 +0100

    better theme copilot chat

--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -1,12 +1,32 @@
+local prompts = {
+  -- Code related prompts
+  Explain = "Please explain how the following code works.",
+  Review = "Please review the following code and provide suggestions for improvement.",
+  Tests = "Please explain how the selected code works, then generate unit tests for it.",
+  Refactor = "Please refactor the following code to improve its clarity and readability.",
+  FixCode = "Please fix the following code to make it work as intended.",
+  FixError = "Please explain the error in the following text and provide a solution.",
+  BetterNamings = "Please provide better names for the following variables and functions.",
+  Documentation = "Please provide documentation for the following code.",
+  SwaggerApiDocs = "Please provide documentation for the following API using Swagger.",
+  SwaggerJsDocs = "Please write JSDoc for the following API using Swagger.",
+  -- Text related prompts
+  Summarize = "Please summarize the following text.",
+  Spelling = "Please correct any grammar and spelling errors in the following text.",
+  Wording = "Please improve the grammar and wording of the following text.",
+  Concise = "Please rewrite the following text to make it more concise.",
+}
+
 return {
   -- I just yoinked the link below
   -- https://github.com/jellydn/lazy-nvim-ide/blob/main/lua/plugins/extras/copilot-chat-v2.lua
   "CopilotC-Nvim/CopilotChat.nvim",
   opts = {
-    question_header = "User ",
-    answer_header = "Copilot ",
+    question_header = "  User ",
+    answer_header = "  Copilot ",
     error_header = "Error ",
     auto_follow_cursor = false,
+    prompts = prompts,
     window = {
       width = 0.3,
     },
