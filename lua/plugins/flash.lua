5471a34ea4e9bb83ea5f8e0e6aee45ad7405188b
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2024-12-28 22:56:24 +0100

    surround + new flash mapping

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/flash.nvim",
+  keys = {
+    { "s", mode = { "n", "x", "o" }, false },
+    { "S", mode = { "n", "x", "o" }, false },
+  },
+  config = true,
+}
---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,35 @@
+
+return {
+  {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+      local opts = LazyVim.opts("mini.surround")
+      local mappings = {
+        -- TODO: make which-key display on pressing `s` key
+        -- { "s", desc = "surround", mode = { "n", "v" } },
+        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+        { opts.mappings.delete, desc = "Delete surrounding" },
+        { opts.mappings.find, desc = "Find right surrounding" },
+        { opts.mappings.find_left, desc = "Find left surrounding" },
+        { opts.mappings.highlight, desc = "Highlight surrounding" },
+        { opts.mappings.replace, desc = "Replace surrounding" },
+        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+      }
+      mappings = vim.tbl_filter(function(m)
+        return m[1] and #m[1] > 0
+      end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+      },
+    },
+  },
+}
