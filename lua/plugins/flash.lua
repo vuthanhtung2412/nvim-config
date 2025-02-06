3ad0e93ffd6aeb4c86cb83dc497daf1585a1491e
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-02-06 21:43:12 +0100

    flash keymap to s explanation

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,6 +1,7 @@
 return {
   "folke/flash.nvim",
   keys = {
+    -- Disable s since it is used for sentence text object
     { "s", mode = { "n", "x", "o" }, false },
     { "S", mode = { "n", "x", "o" }, false },
   },
