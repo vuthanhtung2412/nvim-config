e3ab85b6b231857478297f029afb967883286b8e
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-02-18 00:49:02 +0100

    paste behaviour

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,10 +29,11 @@ if vim.env.SSH_CLIENT then
       ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
+    -- TODO: for now just paste by control V
+    -- paste = {
+    --   ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    --   ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    -- },
   }
 end
 
