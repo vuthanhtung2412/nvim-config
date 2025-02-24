5212676fe57ab476b8297095931ade03e7a61d2f
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-02-24 15:11:22 +0100

    disable vim-go and enable neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  enabled = false,
+  -- enabled = false,
   opts = {
     window = {
       position = "float";
--- lua/plugins/vim-go.lua
+++ lua/plugins/vim-go.lua
@@ -1,5 +1,8 @@
 return {
   "fatih/vim-go",
+  enabled = false,
+  -- This mess up neovim doc hover urghhhh
+  -- I use this only for GoIfErr, I should just create snippet
   config = function()
     vim.g.go_fmt_autosave = 0
   end,
