f489c499a9cc4ba4a47784ad3c2a7062c6e8189f
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-06-02 02:45:06 +0200

    pyright is less intrusive than basedpyright

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,19 +1,118 @@
 if true then return {} end
+---@diagnostic disable-next-line: unused-local
+local function generate_slash_commands()
+  local commands = {}
+  for _, command in ipairs({ "buffer", "file", "help", "symbols" }) do
+    commands[command] = {
+      opts = {
+        provider = LazyVim.pick.picker.name, -- dynamically resolve the provider
+      },
+    }
+  end
+  return commands
+end
 
 return {
-  "olimorris/codecompanion.nvim",
-  config = true,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    enabled = true,
+    opts = {
+      display = {
+        chat = {
+          intro_message = "Welcome to CodeCompanion ✨! Press ? for options",
+          show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+          auto_scroll = false,
+        },
+      },
+      -- strategies = {
+      --   chat = {
+      --     adapter = "anthropic",
+      --     roles = {
+      --       llm = "CodeCompanion",
+      --       user = "Me",
+      --     },
+      --     slash_commands = generate_slash_commands(),
+      --     keymaps = {
+      --       close = {
+      --         modes = {
+      --           n = "q",
+      --         },
+      --         index = 3,
+      --         callback = "keymaps.close",
+      --         description = "Close Chat",
+      --       },
+      --       stop = {
+      --         modes = {
+      --           n = "<C-c",
+      --         },
+      --         index = 4,
+      --         callback = "keymaps.stop",
+      --         description = "Stop Request",
+      --       },
+      --     },
+      --   },
+      -- },
+      -- inline = {
+      --   adapter = "anthropic",
+      -- },
+    },
+    keys = {
+      {
+        "<leader>ac",
+        "<cmd>CodeCompanionActions<cr>",
+        mode = { "n", "v" },
+        noremap = true,
+        silent = true,
+        desc = "CodeCompanion actions",
+      },
+      {
+        "<leader>aa",
+        "<cmd>CodeCompanionChat Toggle<cr>",
+        mode = { "n", "v" },
+        noremap = true,
+        silent = true,
+        desc = "CodeCompanion chat",
+      },
+      {
+        "<leader>ad",
+        "<cmd>CodeCompanionChat Add<cr>",
+        mode = "v",
+        noremap = true,
+        silent = true,
+        desc = "CodeCompanion add to chat",
+      },
+    },
   },
-  keys = {
-    {
-      "<leader>ac",
-      function()
-        require("codecompanion").toggle()
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "olimorris/codecompanion.nvim", "saghen/blink.compat" },
+    event = "InsertEnter",
+    opts = {
+      enabled = function()
+        return vim.bo.buftype ~= "prompt" and vim.b.completion ~= false
       end,
-      desc = "Toggle AI chat",
+      completion = {
+        accept = {
+          auto_brackets = {
+            kind_resolution = {
+              blocked_filetypes = { "typescriptreact", "javascriptreact", "vue", "codecompanion" },
+            },
+          },
+        },
+      },
+      sources = {
+        per_filetype = {
+          codecompanion = { "codecompanion" },
+        },
+      },
     },
   },
 }
