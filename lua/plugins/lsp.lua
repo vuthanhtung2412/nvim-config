bc316583a40aedf2cf151d90ac1053d305965f4a
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-07-02 18:28:47 +0200

    try changing elixirls version

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- For KBRW formation
+      -- elixirls = {
+      --   cmd = { "/Users/tung/.local/share/elixirls-4-elixir-1.11/language_server.sh" }, -- Change this path!
+      -- },
+    },
+  },
+}
