4f0f128b9e193505548dcc7a856cd59bb8b6d1cd
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-07-11 14:23:13 +0200

    open mime file in yazi while edit text file w EDITOR

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -19,11 +19,39 @@ return {
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_file_function = function(chosen_file, config, state)
-      -- Use system open instead of opening in Neovim
-      -- For macOS
-      vim.fn.system("open " .. vim.fn.shellescape(chosen_file))
-      -- For Linux
-      -- vim.fn.system("xdg-open " .. vim.fn.shellescape(chosen_file))
+      -- Popular media file extensions lookup table
+      local media_extensions = {
+        -- Images
+        jpg = true,
+        jpeg = true,
+        png = true,
+        gif = true,
+        webp = true,
+        svg = true,
+        pdf = true,
+        -- Videos
+        mp4 = true,
+        avi = true,
+        mov = true,
+        mkv = true,
+        webm = true,
+        -- Audio
+        mp3 = true,
+        wav = true,
+        flac = true,
+        aac = true,
+        ogg = true,
+      }
+
+      -- Get file extension and check if it's a media file
+      local extension = chosen_file:match("^.+%.(.+)$")
+      if extension and media_extensions[extension:lower()] then
+        -- Use system open for media files
+        vim.fn.system("open " .. vim.fn.shellescape(chosen_file))
+      else
+        -- Use default behavior (open in Neovim) for non-media files
+        vim.cmd("edit " .. vim.fn.fnameescape(chosen_file))
+      end
     end,
     open_for_directories = false,
     keymaps = {
