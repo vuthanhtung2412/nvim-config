9941dddb52bd605f6ed72c266ef5050afca297ad
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-07-11 12:15:28 +0200

    use open file system for yazi instead of open with nvim

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -12,15 +12,22 @@ return {
       -- Open in the current working directory
       "<leader>Y",
       "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory" ,
+      desc = "Open the file manager in nvim's working directory",
     },
   },
   ---@type YaziConfig
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
+    open_file_function = function(chosen_file, config, state)
+      -- Use system open instead of opening in Neovim
+      -- For macOS
+      vim.fn.system("open " .. vim.fn.shellescape(chosen_file))
+      -- For Linux
+      -- vim.fn.system("xdg-open " .. vim.fn.shellescape(chosen_file))
+    end,
     open_for_directories = false,
     keymaps = {
-      show_help = 'g?',
+      show_help = "g?",
     },
   },
 }
