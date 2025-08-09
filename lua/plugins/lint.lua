41516f07b5de07a0fb3dade0feebd0c8bf8281af
UIA <58302728+vuthanhtung2412@users.noreply.github.com>
2025-08-09 10:37:34 +0200

    Update lint.lua

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,6 +2,7 @@
 return {
   "mfussenegger/nvim-lint",
   opts = function(_, opts)
+    -- other useful link : https://github.com/LazyVim/LazyVim/discussions/2268#discussioncomment-7970989
     opts.linters_by_ft.markdown = {} -- remove markdown linting
   end,
 }
