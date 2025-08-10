831ff83eb482f9cc09fbc248262b091bc4b4c060
UIA <58302728+vuthanhtung2412@users.noreply.github.com>
2025-08-10 09:52:00 +0200

    fix relative file path + get coord of selection

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,19 +20,15 @@ vim.api.nvim_set_keymap("o", "il", ":<C-u>normal vil<CR>", { noremap = true, sil
 vim.keymap.set({ "n" }, "oo", "o<ESC>", { noremap = true, silent = true })
 vim.keymap.set({ "n" }, "OO", "O<ESC>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "F", "<nop>", { noremap = true, silent = true })
--- Yank file name
-vim.keymap.set(
-  "n",
-  "<leader>fp",
-  ':let @+ = expand("%")<CR>',
-  { desc = "yank file relative file", noremap = true, silent = true }
-)
-vim.keymap.set(
-  "n",
-  "<leader>fP",
-  ':let @+ = expand("%:p")<CR>',
-  { desc = "yank absolute file path", noremap = true, silent = true }
-)
+-- Yank relative file path to CWD
+vim.keymap.set("n", "<leader>fp", function()
+  vim.fn.setreg("+", vim.fn.fnamemodify(vim.fn.expand("%"), ":."))
+end, { desc = "Yank relative file path", silent = true })
+
+-- Yank absolute file path
+vim.keymap.set("n", "<leader>fP", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p")) -- absolute path
+end, { desc = "Yank absolute file path", silent = true })
 
 -- Keymap only for Terraform files
 vim.api.nvim_create_autocmd("FileType", {
@@ -76,4 +72,28 @@ vim.keymap.set("n", "<leader>fdg", function()
     end
   end)
 end, { desc = "Grep directory" })
--- Tung is here
+
+-- Get current selection position
+vim.keymap.set("v", "<leader>@", function()
+  -- Get current file path relative to CWD
+  local file_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+
+  -- Get start and end line of selection
+  local start_line = vim.fn.line("v")
+  -- FIXME: not resistant to O in selection mode
+  local end_line = vim.fn.line(".") --Line number of the cursor
+
+  -- Ensure correct ordering
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  -- Format string like "@path#Lstart-Lend"
+  local pos_str = string.format("@%s#L%d-L%d", file_path, start_line, end_line)
+
+  -- Copy to system clipboard
+  vim.fn.setreg("+", pos_str)
+
+  -- Notify user
+  print("Copied position: " .. pos_str)
+end, { desc = "Coord of selection", silent = true })
